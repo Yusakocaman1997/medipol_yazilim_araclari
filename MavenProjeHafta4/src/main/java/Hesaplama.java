8db5f0d312e3b185f37d6f03861d3a29c3269b6f
bprlab <bprlab@10.201.32.220>
2018-03-17 14:32:20 +0300

    Commit and

---
+++ MavenProjeHafta4/src/main/java/Hesaplama.java
@@ -0,0 +1,8 @@
+
+public class Hesaplama {
+	
+	public static int kareAlma (int sayi) {
+		return sayi * sayi;
+	}
+	
+}
---
+++ MavenProjeHafta4/src/test/java/HesaplamaTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+
+import org.junit.Assert;
+
+public class HesaplamaTest {
+	
+	@Test
+	
+	public void testKareAlma() {
+		Assert.assertEquals(100,	Hesaplama.kareAlma(10));
+		
+	}
+
+}
